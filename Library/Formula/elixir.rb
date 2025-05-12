48246b65a52f58684ae078a8970dcedb6fe0bcb6
jcgraybill <65628360+jcgraybill@users.noreply.github.com>
2025-05-12 14:51:20 -0700

    elixir: 1.14.5

--- Library/Formula/elixir.rb
+++ Library/Formula/elixir.rb
@@ -1,4 +1,4 @@
-class Erlang17Requirement < Requirement
+class Erlang23Requirement < Requirement
   fatal true
   env :userpaths
   default_formula "erlang"
@@ -6,12 +6,12 @@ class Erlang17Requirement < Requirement
   satisfy do
     erl = which("erl")
     next unless erl
-    `#{erl} -noshell -eval 'io:fwrite("~s~n", [erlang:system_info(otp_release)]).' -s erlang halt | grep -q '^1[789]'`
+    `#{erl} -noshell -eval 'io:fwrite("~s~n", [erlang:system_info(otp_release)]).' -s erlang halt | grep -q '^2[34567]'`
     $?.exitstatus == 0
   end
 
   def message; <<-EOS.undent
-    Erlang 17+ is required to install.
+    Erlang 23+ is required to install.
 
     You can install this with:
       brew install erlang
@@ -25,12 +25,12 @@ end
 class Elixir < Formula
   desc "Functional metaprogramming aware language built on Erlang VM"
   homepage "http://elixir-lang.org/"
-  url "https://github.com/elixir-lang/elixir/archive/v1.5.3.tar.gz"
-  sha256 "0fc6024b6027d87af9609b416448fd65d8927e0d05fc02410b35f4b9b9eb9629"
+  url "https://github.com/elixir-lang/elixir/archive/v1.14.5.tar.gz"
+  sha256 "2ea249566c67e57f8365ecdcd0efd9b6c375f57609b3ac2de326488ac37c8ebd"
 
   head "https://github.com/elixir-lang/elixir.git"
 
-  depends_on Erlang17Requirement
+  depends_on Erlang23Requirement
   # make: *** virtual memory exhausted.  Stop.
   depends_on "make" => :build
 
