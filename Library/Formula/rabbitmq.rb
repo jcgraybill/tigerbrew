1064d1d5693a9a447ea44adb0f376134901b7aff
jcgraybill <65628360+jcgraybill@users.noreply.github.com>
2025-07-02 16:20:20 -0700

    new make command

--- Library/Formula/rabbitmq.rb
+++ Library/Formula/rabbitmq.rb
@@ -5,12 +5,12 @@ class Rabbitmq < Formula
   sha256 "9557f9bc8c0bd21408738def2d8137a76aa044948522493c8757a3a7d8e27fec"
   head "https://github.com/rabbitmq/rabbitmq-server.git"
 
-  depends_on "make" => :build
+  depends_on "simplejson" => :python
   depends_on "elixir" => :build
   depends_on "erlang"
 
   def install
-    system "make", "PREFIX=#{prefix}"
-    system "make", "install"
+    system "PREFIX=#{prefix}", "gmake"
+    system "PREFIX=#{prefix}", "gmake", "install"
   end
 end
