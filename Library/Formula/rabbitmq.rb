a5dcd4f3b0b7bd285d05707e016bf4b4e42bb6fd
jcgraybill <65628360+jcgraybill@users.noreply.github.com>
2025-07-23 12:01:26 -0700

    simple copy install

--- Library/Formula/rabbitmq.rb
+++ Library/Formula/rabbitmq.rb
@@ -1,16 +1,12 @@
 class Rabbitmq < Formula
   desc "Messaging broker"
   homepage "https://www.rabbitmq.com"
-  url "https://github.com/rabbitmq/rabbitmq-server/archive/refs/tags/v3.10.7.tar.gz"
-  sha256 "9557f9bc8c0bd21408738def2d8137a76aa044948522493c8757a3a7d8e27fec"
-  head "https://github.com/rabbitmq/rabbitmq-server.git"
+  url "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.10.7/rabbitmq-server-generic-unix-3.10.7.tar.xz"
+  sha256 "fcb424deb300635086f3bf554063af654bb58f2aa4e84126f514acc00439f5c2"
 
-  depends_on "simplejson" => :python
-  depends_on "elixir" => :build
   depends_on "erlang"
 
   def install
-    system "gmake", "PREFIX=#{prefix}"
-    system "gmake", "install", "PREFIX=#{prefix}"
+    prefix.install Dir["*"]
   end
 end
