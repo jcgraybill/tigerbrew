5fe63204b98d46ee320791c9f92be057e99271b7
jcgraybill <65628360+jcgraybill@users.noreply.github.com>
2025-07-23 17:43:30 -0700

    ejabberd simplify formula and add deps hack

--- Library/Formula/ejabberd.rb
+++ Library/Formula/ejabberd.rb
@@ -4,27 +4,15 @@ class Ejabberd < Formula
   url "https://github.com/processone/ejabberd/archive/refs/tags/25.04.tar.gz"
   sha256 "54beae3e7729fdaab1d578a9d59046f31d8ce31c851ae5aca9532821ff22cb45"
 
-  option "32-bit"
-
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
   depends_on "rebar3" => :build
-
   depends_on "erlang"
-  depends_on "libyaml"
   depends_on "openssl3"
 
   # for CAPTCHA challenges
   depends_on "imagemagick" => :optional
 
   def install
-    ENV["TARGET_DIR"] = ENV["DESTDIR"] = "#{lib}/ejabberd/erlang/lib/ejabberd-#{version}"
-    ENV["MAN_DIR"] = man
-    ENV["SBIN_DIR"] = sbin
-
-    if build.build_32_bit?
-      ENV.append %w[CFLAGS LDFLAGS], "-arch #{Hardware::CPU.arch_32_bit}"
-    end
+    inreplace "Makefile.in", "DEPS:=$(sort $(shell QUIET=1 $(REBAR) $(LISTDEPS) | $(SED) -ne $(DEPSPATTERN) ))", "DEPS:=base64url cache_tab eimp epam ezlib fast_tls fast_xml fast_yaml idna jiffy jose luerl mqtree p1_acme p1_mysql p1_oauth2 p1_pgsql p1_utils pkix stringprep stun unicode_util_compat xmpp yconf"
 
     args = ["--prefix=#{prefix}",
             "--sysconfdir=#{etc}",
@@ -34,50 +22,11 @@ class Ejabberd < Formula
             "--enable-odbc",
             "--enable-pam"]
 
-    system "autoupdate"
     system "./autogen.sh"
     system "./configure", *args
 
     system "make"
     system "make", "install"
 
-    (etc+"ejabberd").mkpath
-    (var+"lib/ejabberd").mkpath
-    (var+"spool/ejabberd").mkpath
-  end
-
-  def caveats; <<-EOS.undent
-    If you face nodedown problems, concat your machine name to:
-      /private/etc/hosts
-    after 'localhost'.
-    EOS
-  end
-
-  plist_options :manual => "#{HOMEBREW_PREFIX}/sbin/ejabberdctl start"
-
-  def plist; <<-EOS.undent
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>EnvironmentVariables</key>
-      <dict>
-        <key>HOME</key>
-        <string>#{var}/lib/ejabberd</string>
-      </dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{opt_sbin}/ejabberdctl</string>
-        <string>start</string>
-      </array>
-      <key>RunAtLoad</key>
-      <true/>
-      <key>WorkingDirectory</key>
-      <string>#{var}/lib/ejabberd</string>
-    </dict>
-    </plist>
-    EOS
   end
 end
