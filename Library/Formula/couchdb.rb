403792cc1b4a4efa84b8bd93f7abeabfaf0b4d18
jcgraybill <65628360+jcgraybill@users.noreply.github.com>
2025-07-25 08:06:14 -0700

    couchdb 3.2.3

--- Library/Formula/couchdb.rb
+++ Library/Formula/couchdb.rb
@@ -1,50 +1,21 @@
 class Couchdb < Formula
   desc "CouchDB is a document database server"
   homepage "https://couchdb.apache.org/"
-  url "https://dlcdn.apache.org/couchdb/source/3.3.3/apache-couchdb-3.3.3.tar.gz"
-  mirror "https://dlcdn.apache.org/couchdb/source/3.3.3/apache-couchdb-3.3.3.tar.gz"
-  sha256 "7a2007b5f673d4be22a25c9a111d9066919d872ddb9135a7dcec0122299bd39e"
-
-  head do
-    url "https://git-wip-us.apache.org/repos/asf/couchdb.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-    depends_on "autoconf-archive" => :build
-    depends_on "pkg-config" => :build
-  end
+  url "https://dlcdn.apache.org/couchdb/source/3.2.3/apache-couchdb-3.2.3.tar.gz"
+  sha256 "c830ea9014177eed9989b160ea92dde79f724519b8e677c4d2e8fca22836e85c"
 
   depends_on "spidermonkey"
-  depends_on "icu4c"
   depends_on "erlang"
-  depends_on "curl" if MacOS.version <= :leopard
-
+  depends_on "icu4c"
+  depends_on "openssl3"
+  depends_on "rebar" => :build
+  depends_on "rebar3" => :build
+  depends_on "help2man" => :build
+  
   def install
-
-    system "./configure", "--rebar3", "#{Formula["rebar3"].opt_bin}/rebar3", 
-                          "--rebar", "#{Formula["rebar"].opt_bin}/rebar"
+    system "./configure"
     system "make", "release"
-
-    # setting new database dir
     inreplace "rel/couchdb/etc/default.ini", "./data", "#{var}/couchdb/data"
-    # remove windows startup script
-    rm("rel/couchdb/bin/couchdb.cmd")
-    # install files
     prefix.install Dir["rel/couchdb/*"]
-  end
-
-  def caveats
-    <<~EOS
-      CouchDB 3.x requires a set admin password set before startup.
-      Add one to your #{etc}/local.ini before starting CouchDB e.g.:
-        [admins]
-        admin = youradminpassword
-    EOS
-  end
-
-  test do
-    # ensure couchdb embedded spidermonkey vm works
-    system "#{bin}/couchjs", "-h"
   end
 end
